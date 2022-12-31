43fed082e18bc9d01e6c41a469ba435dc7729f49
Stephen <sis33@cornell.edu>
2022-12-31 06:19:57 -0500

    Creation of footer and Music page modified

---
+++ main_cmp.js
@@ -0,0 +1,12 @@
+<script>
+$(document).ready(function(){
+$(window).scroll(function(){
+if($(this).scrollTop() > 40){
+    $('#back-to-top').fadeIn();
+} else{
+    $('#back-to-top').fadeOut();
+}})
+
+}("#back-to-top").click(function(){
+$('html')
+}</script>
