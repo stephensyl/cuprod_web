0175853bbc646b5479908e06075fb4c427b6117b
James Koga <jyk54@cornell.edu>
2023-01-05 18:41:06 -0800

    fixing buttons

--- nav-button-script.js
+++ nav-button-script.js
@@ -1,10 +1,10 @@
 const lineupBtn = document.getElementById("lineupBtn");
-const aboutBtn = document.getElementById("aboutBtn");
+//const aboutBtn = document.getElementById("aboutBtn");
 const musicBtn = document.getElementById("musicBtn");
 const eventsBtn = document.getElementById("eventsBtn");
-const contactBtn = document.getElementById("contactBtn");
-const resourcesBtn = document.getElementById("resourcesBtn");
-const logoBtn = document.getElementById("logo");
+//const contactBtn = document.getElementById("contactBtn");
+//const resourcesBtn = document.getElementById("resourcesBtn");
+const logoBtn = document.getElementById("logoBtn");
 
 //debug this below... cant find out why it doesnt work
 function addNavBtn(button, file) {
@@ -15,9 +15,9 @@ function addNavBtn(button, file) {
 }
 
 addNavBtn(lineupBtn, "lineup_cmp.html");
-addNavBtn(aboutBtn, "about_cmp.html");
+//addNavBtn(aboutBtn, "about_cmp.html");
 addNavBtn(logoBtn, "index_cmp.html");
 addNavBtn(musicBtn, "music_cmp.html");
 addNavBtn(eventsBtn, "events_cmp.html");
-addNavBtn(contactBtn, "contact_cmp.html");
-addNavBtn(resourcesBtn, "resources_cmp.html");
+//addNavBtn(contactBtn, "contact_cmp.html");
+//addNavBtn(resourcesBtn, "resources_cmp.html");
