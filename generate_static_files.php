98d5d1f00c0937a3bf4136baa190c775425ff7d2
stephensyl <120346436+stephensyl@users.noreply.github.com>
2024-07-10 17:38:09 +0000

    Update PHP script to capture and convert PHP output to static HTML

--- generate_static_files.php
+++ generate_static_files.php
@@ -1,11 +1,52 @@
 <?php
-$files = ['index.php', 'about.php', 'contact.php', 'events.php', 'lineup.php', 'music.php', 'resources.php', 'footer.php', 'header.php']; // all PHP files here
 
-foreach ($files as $file) {
-    ob_start();
-    include $file;
-    $content = ob_get_clean();
-    $outputFile = str_replace('.php', '.html', $file);
-    file_put_contents('static/' . $outputFile, $content);
+$staticDir = 'static';
+
+// Create the static directory if it doesn't exist
+if (!is_dir($staticDir)) {
+    mkdir($staticDir, 0777, true);
 }
-?>
+
+// Function to capture the output of a PHP file
+function captureOutput($file) {
+    ob_start();
+    include($file);
+    return ob_get_clean();
+}
+
+// Function to create static file with captured output
+function createStaticFile($phpFile, $htmlFile) {
+    global $staticDir;
+    $content = captureOutput($phpFile);
+    $fullPath = $staticDir . '/' . $htmlFile;
+
+    if (file_put_contents($fullPath, $content) === false) {
+        echo "Failed to write to $fullPath\n";
+    } else {
+        echo "Successfully wrote to $fullPath\n";
+    }
+}
+
+// List of PHP files to convert to static HTML
+$filesToConvert = [
+    'index.php' => 'index.html',
+    'about.php' => 'about.html',
+    'contact.php' => 'contact.html',
+    'events.php' => 'events.html',
+    'lineup.php' => 'lineup.html',
+    'music.php' => 'music.html',
+    'resources.php' => 'resources.html',
+    'footer.php' => 'footer.html',
+    'header.php' => 'header.html',
+];
+
+foreach ($filesToConvert as $phpFile => $htmlFile) {
+    if (file_exists($phpFile)) {
+        createStaticFile($phpFile, $htmlFile);
+    } else {
+        echo "$phpFile does not exist.\n";
+    }
+}
+
+echo "Static files generated successfully.\n";
+
