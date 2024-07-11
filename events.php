52e97ecf5467cb77de3a1e1107962956e68daba8
stephensyl <120346436+stephensyl@users.noreply.github.com>
2024-07-11 19:35:01 +0000

    Made events calendar: - user can subscribe to calendar - user can see dates and time

--- events.php
+++ events.php
@@ -3,14 +3,12 @@
     <body>
 
         <center>
-            <a <iframe> <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&title=CORNELL%20MUSIC%20PRODUCTION%20SCHEDULE%20%F0%9F%91%BD&src=Y29ybmVsbG11c2ljcHJvZHVjdGlvbkBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=bmoxcDAyZ2g5ajJ0cXZtcTd0NWlxZnZwazBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cXQzbzc1OGZtbHEyZmtxODdzZGpzc2Z0bW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%23E67C73&color=%237CB342&color=%230B8043"
-                    style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe> Schedule
-                </iframe>
-            </a>
+        <iframe src="https://embed.styledcalendar.com/#X9wPtjEwB5oUFtUqHzcq" title="Styled Calendar" class="styled-calendar-container" style="width: 100%; border: none;" data-cy="calendar-embed-iframe"></iframe>
+            <script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
         </center>
 
     </body>
     <?php include 'includes/footer.php';?>
-<script src="js/nav-button-script.js"></script>
+    <script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
 
 </html>
