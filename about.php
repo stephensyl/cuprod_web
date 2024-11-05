c7d80d5037ff119f540bd892d11b53f32a4e1f6a
stephensyl <120346436+stephensyl@users.noreply.github.com>
2024-11-05 00:12:47 +0000

    Pushing more content on about page

--- about.php
+++ about.php
@@ -2,6 +2,59 @@
 <body>
     <main>
 
+    <div id="history">
+        <h2>Our History</h2>
+        <p>Founded in 2020, It started as a small club meeting in Uris hall. By 2021, we pushed to expand the genres and population of our club by having more social activites. 
+            Now we can boast an enrollment of over 260 undergrad student members and 2 Genre Rich Albums. Welcome to our story and these are are that make it happen: </p>
+    </div>
+
+    <div id="eboard">
+        <div id="chair">
+        <h2>Our E-Board</h2>
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Co-President</h3>
+            <p>James Koga</p>
+        </div>
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Co-President</h3>
+            <p>Stephen Syl-Akinwale</p>
+        </div>
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Treasurer</h3>
+            <p>Facundo Torres</p>
+        </div>
+        </div>
+        
+
+        <div id="creative">
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Social Media Manager</h3>
+            <p>Chelsea Baez</p>
+        </div>
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Media Management</h3>
+            <p>Anthony Lewis</p>
+        </div>
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Club Designer</h3>
+            <p>Diego Onofri</p>
+        </div>
+        <div id="club-relations">
+        <div class="eboard-member">
+            <img src="images/placeholder.png" alt="placeholder">
+            <h3>Club Relations</h3>
+            <p>Charolette</p>
+        </div>
+        </div>
+    </div>
+
+
     </main>
     <?php include 'includes/footer.php';?>
 </body>
